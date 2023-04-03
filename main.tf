02fc9e58ea6f7912c0669b267c314f5764b32e70
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-03 08:43:18 +0000

    exc

--- main.tf
+++ main.tf
@@ -6,22 +6,45 @@ resource "aws_vpc" "main" {
   ) 
 }
 
-
+## Pubic Route Table
+   
+ resource "aws_route_table" "public_route_talbe" {
+  vpc_id = aws_vpc.main.id
+  
+  for_each = var.public_subnets
+  tags = merge(
+  var.tags,
+  { Name = "${var.env}-${each.value["name"]}" }
+   )
+}
+  
 ## Public Subnets
 
 resource "aws_subnet" "public_subnets" {
   
   vpc_id = aws_vpc.main.id
-  tags = merge(
-  var.tags,
-  { Name = "${var.env}-${each.value["name"]}" }
-   ) 
-   
+  
    for_each = var.public_subnets
    cidr_block = each.value["cidr_block"]
    availability_zone = each.value["availability_zone"]
+   tags = merge(
+   var.tags,
+  { Name = "${var.env}-${each.value["name"]}" }
+   ) 
+ }
+ 
+## Private Route Table
    
-  }
+ resource "aws_route_table" "private_route_talbe" {
+  vpc_id = aws_vpc.main.id
+  
+  for_each = var.private_subnets
+  tags = merge(
+  var.tags,
+  { Name = "${var.env}-${each.value["name"]}" }
+   )
+}
+ 
 ## Private Subnets
 
 resource "aws_subnet" "private_subnets" {
